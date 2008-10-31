b05f48392465db1b01366744b07937b2ffec0ec3
lhaagounet <lhaagounet@fbdba616-8651-11dd-90a9-6306d3ffb32f>
2008-10-31 13:38:31 +0000

    Phase de transition 1

--- JiGoLo/src/com/go/board/StandardBoard.java
+++ JiGoLo/src/com/go/board/StandardBoard.java
@@ -3,11 +3,9 @@
  */
 package com.go.board;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.go.board.boardData.BoardData;
+import com.go.board.intersection.StandardIntersection;
 import com.go.game.move.Move;
+import com.go.rules.color.Color;
 
 /**
  * @author LhaaG
@@ -17,7 +15,7 @@ public class StandardBoard implements GenericBoard {
 
 	private int hSize;
 	private int vSize;
-	private Map<Integer, BoardData> gobanMap;
+	private StandardIntersection[][] currentGoban;
 	
 	/**
 	 * Constructeur par défaut en 19*19
@@ -39,17 +37,22 @@ public class StandardBoard implements GenericBoard {
 	}
 
 	private void init() {
-		this.gobanMap = new HashMap<Integer, BoardData>();
-		this.gobanMap.put(0, new BoardData(this.hSize, this.vSize));
+		this.currentGoban = new StandardIntersection[this.hSize][this.vSize];
+		for (int i=0; i<this.hSize; i++) {
+			for (int j=0; j<this.vSize; j++) {
+				StandardIntersection emptyCase = new StandardIntersection(Color.getEMPTY(), i, j);
+				this.currentGoban[i][j] = emptyCase;
+			}
+		}
 	}
 
-	public void playMoveAt(Move move) {
-		BoardData newBoard = new BoardData(move, this.getGobanAt(move.getNumber()).getIntersectionArray());
-		this.gobanMap.put(move.getNumber(), newBoard);
+	public void playMove(Move move) {
+		if (!move.isPassMove())
+			this.currentGoban[move.getHorizontalCoord()][move.getVerticalCoord()].setColor(move.getColor());
 	}
 
-	public BoardData getGobanAt(int move) {
-		return this.gobanMap.get(move);
+	public StandardIntersection getIntersectionAt(int hCoord, int vCoord) {
+		return this.currentGoban[hCoord][vCoord];
 	}
 
 }// class StandardBoard
---
+++ JiGoLo/src/com/gui/goban/manager/JGobanComponent.java
@@ -0,0 +1,17 @@
+/**
+ * Créée le 3 oct. 2008
+ */
+package com.gui.goban.manager;
+
+import javax.swing.JComponent;
+
+/**
+ * @author LhaaG
+ *
+ */
+public abstract class JGobanComponent extends JComponent {
+
+	
+	
+
+}// abstract class JGobanComponent
